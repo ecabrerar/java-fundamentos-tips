9b7cb298633d4eef22d7ac28aacc99bc839b342b
Eudris Cabrera <eudris@gmail.com>
2024-03-08 13:30:13 -0400

    basic examples

---
+++ basic-programming-principles/finish/basic-programming-principles/src/main/java/com/eudriscabrera/examples/java/Book.java
@@ -0,0 +1,58 @@
+package com.eudriscabrera.examples.java;
+
+/**
+ * 
+ * @author ecabrerar
+ * @since Mar 8, 2024
+ */
+public class Book {
+    private String isbn;
+    private String title;
+    private String author;
+    private String publisher;
+    private String edition;
+    private double price;
+    private String description;
+    
+	public Book(String isbn, String title, String author, String publisher, String edition, double price,
+			String description) {
+		super();
+		this.isbn = isbn;
+		this.title = title;
+		this.author = author;
+		this.publisher = publisher;
+		this.edition = edition;
+		this.price = price;
+		this.description = description;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public String getEdition() {
+		return edition;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public String getDescription() {
+		return description;
+	}    
+    
+
+}
---
+++ basic-programming-principles/start/basic-programming-principles/src/main/java/com/eudriscabrera/examples/java/Invoice.java
@@ -0,0 +1,43 @@
+package com.eudriscabrera.examples.java;
+
+/**
+ * 
+ * @author ecabrerar
+ * @since Mar 8, 2024
+ */
+public class Invoice {
+	private Book book;
+	private int quantity;
+	private double discountRate;
+	private double taxRate;
+	private double total;
+
+	public Invoice(Book book, int quantity, double discountRate, double taxRate) {
+		super();
+		this.book = book;
+		this.quantity = quantity;
+		this.discountRate = discountRate;
+		this.taxRate = taxRate;
+		this.total = calculateTotal();
+	}
+
+	public double calculateTotal() {
+
+		double price = (book.getPrice() - (book.getPrice() * discountRate)) * quantity;
+		
+		return  price * (1+taxRate);
+		
+	}
+	
+	public void printInvoice() {
+		  System.out.println(quantity + "x " + book.getTitle() + " " + book.getPrice() + "$");
+          System.out.println("Discount Rate: " + discountRate);
+          System.out.println("Tax Rate: " + taxRate);
+          System.out.println("Total: " + total);
+	}
+	
+	public void saveInvoice(String fileName) {
+		//save the invoice
+	}
+
+}
